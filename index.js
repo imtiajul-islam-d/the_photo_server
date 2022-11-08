0ea93f8b1ab84bd135d5de9ed19786a2ef380313
imtiajul-islam-d <imtiajul@hotmail.com>
2022-11-08 08:05:29 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,10 +14,25 @@ app.use('*', (req, res, next) => {
     next()
 })
 // custom middleware end
+
+// mongodb start
+// connect mongodb
+const uri = process.env.DB_USER;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// mongodb connected
+// mongodb end
+
 // middleware end
 app.get('/', (req, res) => {
     res.send('Server connected')
 })
 app.listen(port, () => {
-    console.log('Server connected')
+    client.connect(err => {
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Mongodb connected")
+        }
+    });
+    console.log('Server connected', process.env.PORT)
 })
