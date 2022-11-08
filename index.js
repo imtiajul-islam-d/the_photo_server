3fc3f42874f9644770b39216edf7688e42e38c2c
imtiajul-islam-d <imtiajul@hotmail.com>
2022-11-08 07:49:27 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware start
+app.use(cors());
+app.use(express.json())
+// custom middleware start
+app.use('*', (req, res, next) => {
+    console.log(req.url);
+    next()
+})
+// custom middleware end
+// middleware end
+
